924edbc82f80c807cde2cfb67574c197d9d7c58e
Travis CI <travis@travis-ci.org>
2019-08-07 11:22:41 -0400

    fix deploy

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,9 +2,9 @@
 set -e
 
 echo Publish: Checking for changes...
-PUBLISHED_VERSION=`npm view node-server-utils version`
+PUBLISHED_VERSION=`npm view mongo-koa-router version`
 npm version --no-git-tag-version $PUBLISHED_VERSION
-PUBLISHED_SHA=`npm view node-server-utils --json | jq .dist.shasum`
+PUBLISHED_SHA=`npm view mongo-koa-router --json | jq .dist.shasum`
 NEW_SHA=`npm publish --dry-run --json | jq .shasum`
 if [ "$PUBLISHED_SHA" != "$NEW_SHA" ]; then 
     echo Publish: Publishing...
